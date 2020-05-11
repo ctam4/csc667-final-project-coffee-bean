b9887fc61405e97f404ab619721e3a8926d8d41e
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-10 20:45:53 -0700

    Fix mongodb with async

--- backend/api/mongodb.js
+++ backend/api/mongodb.js
@@ -3,7 +3,13 @@ const mongodb = require('mongodb');
 const mongodbOptions = {
   host: process.env.MONGODB_HOST || 'localhost',
   port: process.env.MONGODB_PORT || '27017',
-  db: (process.env.MONGODB_DATABASE && '/') || '',
+  db: (process.env.MONGODB_DATABASE && `/${process.env.MONGODB_DATABASE}`) || '',
 };
 
-module.exports = mongodb.MongoClient.connect(`mongodb://${mongodbOptions.host}:${mongodbOptions.port}${mongodbOptions.db}`);
+module.exports = (async () => {
+  const connection = await new mongodb.MongoClient(`mongodb://${mongodbOptions.host}:${mongodbOptions.port}`, { useNewUrlParser: true }).connect();
+  return {
+    connection,
+    db: connection.db(mongodbOptions.db),
+  };
+})();
