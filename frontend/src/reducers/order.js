b746a58ed4dc3bde79c1d3eb7ed5be65da11c349
Brian Nguyen <brianlnguyen12@gmail.com>
2020-05-11 15:37:54 -0700

    Update order.js

--- frontend/src/reducers/order.js
+++ frontend/src/reducers/order.js
@@ -1,7 +1,4 @@
-import {
-  ORDER_FETCH_ORDERS,
-  ORDER_FETCH_ORDER,
-} from '../actions/types';
+import { ORDER_FETCH_ORDERS, ORDER_FETCH_ORDER, } from '../actions/types';
 
 // so orderHistory is an object that stores amount of orders the user has purchased and each order index represents and order of that time.
 // maybe needs to be refactored to have orders represent an object instead of an array?
