c3f0741a6dd47fdc4e6e81485826244dc01f3325
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-10 21:02:08 -0700

    Removed not used code

--- backend/api/routes/root.js
+++ backend/api/routes/root.js
@@ -2,12 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-/*
-const redis = require('../redis.js');
-const kafka = require('../kafka.js');
-const mongodb = require('../mongodb.js');
-*/
-
 router.get('/', (req, res) => res.send('Hello World!'));
 
 module.exports = router;
