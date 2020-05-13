117e1ff546fab03e62d09c14da5851d2db1e8202
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-13 00:34:56 -0700

    ESLint fixes

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -7,9 +7,9 @@ import Home from './Home';
 
 const Routes = () => (
   <Switch>
-    <Route path='/' exact component={Home} />
-    <Route path='/signup' exact component={SignUp} />
-    <Route path='/login' exact component={LogIn} />
+    <Route path="/" exact component={Home} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route path="/login" exact component={LogIn} />
   </Switch>
 );
 
