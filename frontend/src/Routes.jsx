853ee1795546663ce36bfb4dfe43259fd69d9889
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-12 20:00:58 -0700

    Import login from HW3 (#26)

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import LogIn from './LogIn';
 
 const Routes = () => (
   <Switch>
+    <Route path='/login' exact component={LogIn} />
   </Switch>
 );
 
