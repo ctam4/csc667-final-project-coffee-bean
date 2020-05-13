adc6e2dde379d35ce1e9e8ee723250d6b5445af0
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-13 01:31:02 -0700

    Frontend signup (#30)
    * add sign up ui
    
    * Code reformat
    
    * update signup for calling backend
    
    * update signup to call backend
    
    * fix mirror error for sign up
    
    * fix quote
    
    * simplify sign up
    
    * fix redux
    
    * ESLint fixes
    
    * Fix merge error
    
    * Remove auth reducer, email password from user reducers
    
    * Fix ESLint errors
    
    * Fix camel case url, Replace nextPage for redirect
    
    * Replace double quotes to single
    
    * Fix SignUp
    
    * ESLint fix
    
    * Renamed function, removed not used code
    
    * Fix broken merge
    
    * Patch fbb954
    
    * Merge code from login
    
    * Fix url for integration
    
    * Fix duplicated imports
    
    Co-authored-by: jason1819 <jxie3@mail.sfsu.edu>

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
+
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Avatar, Button, TextField, Link, Grid, Typography, Container,
+  Avatar, Button, TextField, Link, Grid, Typography, Container, Paper,
 } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Redirect } from 'react-router-dom';
+
 import apiUrl from '../api';
 
-const styles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
     display: 'flex',
     alignItems: 'center',
     marginTop: theme.spacing(5),
     flexDirection: 'column',
+    padding: theme.spacing(3),
   },
   avatar: {
     margin: theme.spacing(1),
@@ -28,7 +32,8 @@ const styles = makeStyles((theme) => ({
 }));
 
 const SignUp = () => {
-  const classes = styles();
+  const classes = useStyles();
+  const [cookies, setCookie] = useCookies(['token']);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState('');
@@ -36,7 +41,7 @@ const SignUp = () => {
   const handleSubmission = async (e) => {
     e.preventDefault();
     if (email.length > 0 && password.length > 0) {
-      await fetch(`${apiUrl}/auth/signup`, {
+      await fetch(`${apiUrl}auth/signup`, {
         method: 'post',
         headers: {
           Accept: 'application/json',
@@ -61,30 +66,32 @@ const SignUp = () => {
     return <Redirect to={redirect} />;
   }
   return (
-    <Container component="main" maxWidth="sm">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">Sign-up</Typography>
-        <form className={classes.form} onSubmit={(e) => handleSubmission(e)}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField variant="outlined" required fullWidth name="email" label="Email Address" type="email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    <React.Fragment>
+      <Container component="main" maxWidth="sm">
+        <Paper className={classes.paper} elevation={0}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">Sign-up</Typography>
+          <form className={classes.form} onSubmit={(e) => handleSubmission(e)}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField variant="outlined" required fullWidth name="email" label="Email Address" type="email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField variant="outlined" required fullWidth name="password" label="Password" type="password" autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </Grid>
+              <Grid item xs={12}>
+                <Button type="submit" fullWidth variant="contained" color="primary">Sign Up</Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Link to="/login" variant="body2">Already have an account? Log in now</Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField variant="outlined" required fullWidth name="password" label="Password" type="password" autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </Grid>
-            <Grid item xs={12}>
-              <Button type="submit" fullWidth variant="contained" color="primary">Sign Up</Button>
-            </Grid>
-            <Grid item xs={12}>
-              <Link to="/login" href="/login" variant="body2">Already have an account? Sign in now</Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </Paper>
+      </Container>
+    </React.Fragment>
   );
 };
 
