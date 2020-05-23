c25cb210f71c5b92d458607f6fce3c708f1d825f
bdiaz666 <47260214+bdiaz666@users.noreply.github.com>
2020-05-22 23:44:24 -0700

    Frontend home (#76)
    * home js files are added to tgis branch, in order to use them, u need to set them up in Routes file from develop branch
    
    * Merge routes
    
    * Code reformat
    
    * Patch 5e71e0
    
    * Fix missing dependencies
    
    * Add Home to App.js
    
    * Fix inconsist code
    
    * Removed navbar files
    
    * reorganized and rename folder extentions
    
    * reformat and deleted duplicated files
    
    * Fix folder structure
    
    * Rename files and path fixes
    
    * ESLint fixes
    
    * Fix merge error
    
    * Update content at Item1, Item2 component at Home
    
    * Fix merge error
    
    * added dummy data in case there is no data in our database in home page
    
    * Fix uploadData() at Home
    
    Co-authored-by: Calvin Tam <51683307+ctam4@users.noreply.github.com>

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Container, Grid } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import apiUrl from '../api';
 
 import MainPic1 from './components/home/Item1';
 import MainPic2 from './components/home/Item2';
@@ -14,6 +16,79 @@ const useStyles = makeStyles((theme) => ({
 
 export default function RecipeReviewCard() {
   const classes = useStyles();
+
+  const uploadData = async () => {
+    const dataDump = [
+      {
+        "name": "Decaf Pike",
+        "price": 3,
+        "image": "https://globalassets.starbucks.com/assets/f12bc8af498d45ed92c5d6f1dac64062.jpg?impolicy=1by1_wide_1242",
+      },
+      {
+        "name": "Caffe Americano",
+        "price": 4,
+        "image": "https://globalassets.starbucks.com/assets/f12bc8af498d45ed92c5d6f1dac64062.jpg?impolicy=1by1_wide_1242"
+      },
+      {
+        "name": "Americano Blonde",
+        "price": 3,
+        "image": "https://globalassets.starbucks.com/assets/f12bc8af498d45ed92c5d6f1dac64062.jpg?impolicy=1by1_wide_1242",
+      },
+      {
+        "name": "Blonde Roast",
+        "price": 2,
+        "image": "https://globalassets.starbucks.com/assets/f12bc8af498d45ed92c5d6f1dac64062.jpg?impolicy=1by1_wide_1242",
+      },
+      {
+        "name": "Caffè Misto",
+        "price": 4,
+        "image": "https://globalassets.starbucks.com/assets/d668acbc691b47249548a3eeac449916.jpg?impolicy=1by1_wide_1242"
+      },
+      {
+        "name": "Decaf Pike",
+        "price": 2,
+        "image": "https://globalassets.starbucks.com/assets/f12bc8af498d45ed92c5d6f1dac64062.jpg?impolicy=1by1_wide_1242"
+      },
+      {
+        "name": "Cappuccino blond",
+        "price": 3,
+        "image": "https://d3mrtwiv4dr09z.cloudfront.net/media/catalog/product/cache/2/image/720x540/9df78eab33525d08d6e5fb8d27136e95/1/8/18beverage_Cappuccino_720x540.jpg"
+      },
+      {
+        "name": "Cappuccino",
+        "price": 3,
+        "image": "https://d3mrtwiv4dr09z.cloudfront.net/media/catalog/product/cache/2/image/720x540/9df78eab33525d08d6e5fb8d27136e95/1/8/18beverage_Cappuccino_720x540.jpg"
+      },
+      {
+        "name": "Expresso pana",
+        "price": 4,
+        "image": "https://globalassets.starbucks.com/assets/ec519dd5642c41629194192cce582135.jpg?impolicy=1by1_wide_1242"
+      },
+      {
+        "name": "Expresso",
+        "price": 3,
+        "image": "https://globalassets.starbucks.com/assets/e9330b18ae524e69bdcbe97460d6f5bb.jpg?impolicy=1by1_wide_1242",
+      }
+    ];
+    try {
+      const response = await Axios.get(`${apiUrl}product`)
+      if (response.status === 200) {
+        if (response.data.length === 0) {
+          await Promoise.all([
+            dataDump.forEach(async (product) => {
+              await Axios.post(`${apiUrl}product`, product);
+            }),
+          ]);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    uploadData();
+  }, []);
 
   return (
     <>
