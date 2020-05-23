26c8edd7b20b2372f62dea980ccd258dcef651ad
Brian Nguyen <brianlnguyen12@gmail.com>
2020-05-22 19:42:25 -0700

    Update PrivateRoute.js

--- frontend/src/components/privateroute/PrivateRoute.js
+++ frontend/src/components/privateroute/PrivateRoute.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
 import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import apiUrl from '../../../api';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const [cookies] = useCookies(['isLoggedIn']);
-
+const PrivateRoute = ({ component: Component, type, ...rest }) => {
+  const [cookies, setCookie] = useCookies(['isLoggedIn', 'token', 'role']);
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}auth`, { params: { token: cookies.token } })
+      .then((res) => setCookie('role', res.data.role));
+  }, []);
+  console.log(type);
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (cookies.isLoggedIn) {
+        if (
+          cookies.isLoggedIn
+          && cookies.role === 'buyer'
+          && type === 'buyer'
+        ) {
+          return <Component {...props} />;
+        }
+        if (
+          cookies.isLoggedIn
+          && cookies.role === 'seller'
+          && type === 'seller'
+        ) {
           return <Component {...props} />;
         }
         return <Redirect to={{ pathname: '/login' }} />;
